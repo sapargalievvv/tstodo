4f5407a7899123ed7f3b9eeaf06b399d2e43c7f8
sapargalievvv <sapargalievbektas071@gmail.com>
2024-03-04 20:41:52 +0600

    first commit

---
+++ src/components/App.tsx
@@ -0,0 +1,70 @@
+import React, { useState, useEffect, useRef } from "react";
+import { ITodo } from "../types/data";
+import { TodoList } from "./TodoList";
+
+const App: React.FC = () => {
+  const [value, setValue] = useState("");
+  const [todos, setTodos] = useState<ITodo[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const addTodo = () => {
+    if (value) {
+      setTodos([
+        ...todos,
+        {
+          id: Date.now(),
+          title: value,
+          complete: false,
+        },
+      ]);
+
+      console.log(value);
+      setValue("");
+    }
+  };
+  
+  const removeTodo = (id:number): void => {
+    setTodos(todos.filter(todo => todo.id !== id))
+  }
+  
+  const toggleTodo = (id:number):void => {
+    setTodos(todos.map(todo => {
+      if(todo.id !== id) return todo;
+
+      return{
+        ...todo,
+        complete: !todo.complete
+      }
+     }))
+  }
+  
+  useEffect(() => {
+    if(inputRef.current) inputRef.current.focus();
+  },[])
+
+  const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+    setValue(e.target.value)
+  }
+
+  const handleKeyDown:React.KeyboardEventHandler<HTMLInputElement> = (e) => {
+    if(e.key === "Enter") addTodo();  
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="text"
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          ref={inputRef}
+        />
+        <button onClick={addTodo}>Add</button>
+      </div>
+      <TodoList items={todos} removeTodo={removeTodo} toggleTodo={toggleTodo} />
+    </div>
+  );
+};
+
+export { App };
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { ITodo } from "../types/data";
+
+interface ITodoItem extends ITodo {
+  removeTodo: (id: number) => void;
+  toggleTodo: (id: number) => void;
+}
+
+export const TodoItem: React.FC<ITodoItem> = (props) => {
+  const { id, title, complete, removeTodo, toggleTodo } = props;
+
+  return (
+    <div>
+      <input
+        type="checkbox"
+        checked={complete}
+        onChange={() => toggleTodo(id)}
+      />
+      <span style={{
+        display:"inline-block",
+        margin:"0 10px"
+      }}>{title}</span>
+      <button 
+        onClick={() => removeTodo(id)}
+        style={{
+          background:"transparent",
+          border:"none",
+          outline: "none",
+          color:"red",
+        }}
+      >x</button>
+    </div>
+  );
+};
